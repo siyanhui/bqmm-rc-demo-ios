9db6449a4007fc9e57f7d480030d60770421845d
isan <3isan333@gmail.com>
2016-10-24 18:15:20 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sealtalk-ios
-iOS App of SealTalk powered by RongCloud. 基于融云开发的桌面版即时通讯（IM）应用程序 - 嗨豹。
+# 说明：
+- 将appDelegate中[[RCIM sharedRCIM] initWithAppKey:RONGCLOUD_IM_APPKEY BQMMAppId:appId BQMMAppSecret:secret]设置成分配到的id和secret。
+- 本Demo在融云官方2.7.3 Demo的基础上集成了BQMM1.7.2。
--- rongCloud_2.7.3/RCloudMessage/AppDelegate.m
+++ rongCloud_2.7.3/RCloudMessage/AppDelegate.m
@@ -85,8 +85,8 @@
     //初始化融云SDK
 //    [[RCIM sharedRCIM] initWithAppKey:RONGCLOUD_IM_APPKEY];
       
-      NSString *appId = @"15e0710942ec49a29d2224a6af4460ee";
-      NSString *secret = @"b11e0936a9d04be19300b1d6eec0ccd5";
+      NSString *appId = @"appId";
+      NSString *secret = @"secret";
       [[RCIM sharedRCIM] initWithAppKey:RONGCLOUD_IM_APPKEY BQMMAppId:appId BQMMAppSecret:secret];
       //设置区域和语言
       [[MMEmotionCentre defaultCentre] setSdkRegion:MMRegionChina];
